6cd68c28b3b38641d80006eeba0a21e47e27c18a
Nathan Kidd <nathankidd@hey.com>
2026-07-04 00:36:57 +0100

    .

--- flake.nix
+++ flake.nix
@@ -10,22 +10,11 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         ruby = pkgs.ruby_3_4; # Specify version
-        kubectlWithKubeconfig = pkgs.writeShellScriptBin "kubectl" ''
-          #!${pkgs.bash}/bin/bash
-          KUBECONFIG="$PWD/kubeconfig.yaml" ${pkgs.kubectl}/bin/kubectl "$@"
-        '';
       in
       {
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [
-            pkgs.pkg-config # native extension discovery
-          ];
-
           buildInputs = [
             ruby
-            pkgs.libyaml # psych gem
-            pkgs.openssl # openssl gem
-            kubectlWithKubeconfig
           ];
 
           shellHook = ''
@@ -34,7 +23,6 @@
             export PATH="$GEM_HOME/bin:$PATH"
             export BUNDLE_PATH="$GEM_HOME"
             export BUNDLE_BIN="$GEM_HOME/bin"
-            export KUBECONFIG="$PWD/kubeconfig.yaml"
           '';
         };
       }
